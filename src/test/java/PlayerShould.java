d90593c63e42858ad11d0d22f5d7eae7cc7aff63
Domingo Espinosa <domingo.espinosa@codurance.com>
2019-06-27 15:14:04 +0200

    Iteration 2-Refactor

--- src/test/java/PlayerShould.java
+++ src/test/java/PlayerShould.java
@@ -1,32 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.*;
 import static org.hamcrest.core.Is.is;
 
 public class PlayerShould {
+
+    private Player playerOne;
+    private Player playerTwo;
+
+    @BeforeEach
+    void setUp() {
+        playerOne = new Player();
+        playerTwo = new Player();
+    }
+
     @Test
     public void be_created() {
-        Player player = new Player();
-        assertThat(player.health(), is(1000));
-        assertThat(player.level(), is(1));
-        assertThat(player.isAlive(), is(true));
+        assertThat(playerOne.health(), is(1000));
+        assertThat(playerOne.level(), is(1));
+        assertThat(playerOne.isAlive(), is(true));
     }
 
     @Test
     public void deal_damage_to_other_player() {
-        Player playerOne = new Player();
-        Player playerTwo = new Player();
         playerOne.dealDamage(playerTwo, 2);
         assertThat(playerTwo.health(), is(998));
     }
 
     @Test
     public void kill_to_other_player() {
-        Player playerOne = new Player();
-        Player playerTwo = new Player();
         playerOne.dealDamage(playerTwo, 900);
         playerOne.dealDamage(playerTwo, 101);
         assertThat(playerTwo.health(), is(0));
@@ -36,18 +40,14 @@ public class PlayerShould {
     @Test
     @Disabled
     public void heal_to_other_player() {
-        Player player = new Player();
-        Player playerTwo = new Player();
-        player.dealDamage(playerTwo, 100);
-        player.heal(playerTwo, 99);
+        playerOne.dealDamage(playerTwo, 100);
+        playerOne.heal(playerTwo, 99);
         assertThat(playerTwo.health(), is(999));
         assertThat(playerTwo.isAlive(), is(true));
     }
 
     @Test
     public void not_heal_to_player_dead() {
-        Player playerOne = new Player();
-        Player playerTwo = new Player();
         playerOne.dealDamage(playerTwo, 1000);
         playerOne.heal(playerTwo, 10);
         assertThat(playerTwo.health(), is(0));
@@ -57,8 +57,6 @@ public class PlayerShould {
     @Test
     @Disabled
     public void not_heal_over_1000_health() {
-        Player playerOne = new Player();
-        Player playerTwo = new Player();
         playerOne.dealDamage(playerTwo, 1);
         playerOne.heal(playerTwo, 200);
         assertThat(playerTwo.health(), is(1000));
@@ -67,8 +65,6 @@ public class PlayerShould {
     @Test
     @Disabled
     public void heal_200_points() {
-        Player playerOne = new Player();
-        Player playerTwo = new Player();
         playerOne.dealDamage(playerTwo, 900);
         playerOne.heal(playerTwo, 200);
         assertThat(playerTwo.health(), is(300));
@@ -76,21 +72,16 @@ public class PlayerShould {
 
     @Test
     public void not_damage_to_himself() {
-        Player player = new Player();
-        player.dealDamage(player, 500);
-        assertThat(player.health(), is(1000));
+        playerOne.dealDamage(playerOne, 500);
+        assertThat(playerOne.health(), is(1000));
     }
 
     @Test
     public void heal_to_himself() {
-        Player playerOne = new Player();
-        Player playerTwo = new Player();
         playerTwo.dealDamage(playerOne, 200);
         playerOne.heal(playerOne, 100);
         assertThat(playerOne.health(), is(900));
     }
-
     
-
 
 }
