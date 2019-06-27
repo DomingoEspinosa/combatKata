62385d85102996508a68942236618e821729ea29
Domingo Espinosa <domingo.espinosa@codurance.com>
2019-06-27 10:34:14 +0200

    initialize a player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Player {
+    public int healt() {
+        return 1000;
+    }
+
+    public int level() {
+        return 1;
+    }
+
+    public boolean isAlive() {
+        return true;
+    }
 }
--- src/test/java/CombatShould.java
+++ src/test/java/CombatShould.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class CombatShould {
+    @Test
+    public void initializeAPlayer() {
+        Player player = new Player();
+        assertThat(player.healt(), is(1000));
+        assertThat(player.level(), is(1));
+        assertThat(player.isAlive(), is(true));
+    }
 }
